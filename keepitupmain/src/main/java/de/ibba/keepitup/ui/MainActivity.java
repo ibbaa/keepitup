2a00f8bc9fcdd3aa1af3bcc2239d53bdd802a105
Alwin <alwin.ibba@web.de>
2018-12-27 18:40:04 +0100

    settings angepasst

--- keepitupmain/src/main/java/de/ibba/keepitup/ui/MainActivity.java
+++ keepitupmain/src/main/java/de/ibba/keepitup/ui/MainActivity.java
@@ -2,6 +2,7 @@ package de.ibba.keepitup.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import de.ibba.keepitup.R;
 
@@ -11,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
