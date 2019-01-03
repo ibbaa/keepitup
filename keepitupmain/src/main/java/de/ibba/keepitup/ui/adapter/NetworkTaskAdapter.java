a59be5be7721979173a25e91343af6e3dbbc2177
Alwin Ibba <alwin@ibba.de>
2019-01-03 17:54:57 +0100

    UI tweaking, list view continued

--- keepitupmain/src/main/java/de/ibba/keepitup/ui/adapter/NetworkTaskAdapter.java
+++ keepitupmain/src/main/java/de/ibba/keepitup/ui/adapter/NetworkTaskAdapter.java
@@ -1,5 +1,7 @@
 package de.ibba.keepitup.ui.adapter;
 
+import android.content.Context;
+import android.content.res.Resources;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -10,13 +12,16 @@ import java.util.List;
 
 import de.ibba.keepitup.R;
 import de.ibba.keepitup.model.NetworkTask;
+import de.ibba.keepitup.service.NetworkKeepAliveServiceScheduler;
 
 public class NetworkTaskAdapter extends RecyclerView.Adapter<NetworkTaskViewHolder> {
 
     private final List<NetworkTask> networkTasks;
+    private final Context context;
 
-    public NetworkTaskAdapter(List<NetworkTask> networkTasks) {
+    public NetworkTaskAdapter(List<NetworkTask> networkTasks, Context context) {
         this.networkTasks = networkTasks;
+        this.context = context;
     }
 
     @NonNull
@@ -28,12 +33,28 @@ public class NetworkTaskAdapter extends RecyclerView.Adapter<NetworkTaskViewHold
 
     @Override
     public void onBindViewHolder(@NonNull NetworkTaskViewHolder networkTaskViewHolder, int position) {
-//        NetworkTask networkTask = networkTasks.get(position);
-//        networkTaskViewHolder.setTitle(networkTask.getAddress());
+        NetworkTask networkTask = networkTasks.get(position);
+        NetworkKeepAliveServiceScheduler scheduler = new NetworkKeepAliveServiceScheduler(getContext());
+        boolean isRunning = scheduler.isRunning(networkTask);
+        bindStatus(networkTaskViewHolder, isRunning);
+    }
+
+    private void bindStatus(@NonNull NetworkTaskViewHolder networkTaskViewHolder, boolean isRunning) {
+        String statusRunning = isRunning ? getResources().getString(R.string.string_running) : getResources().getString(R.string.string_stopped);
+        String statusText = String.format(getResources().getString(R.string.text_list_item_network_task_status), statusRunning);
+        networkTaskViewHolder.setStatus(statusText);
     }
 
     @Override
     public int getItemCount() {
         return networkTasks.size();
+    }
+
+    private Context getContext() {
+        return context;
+    }
+
+    private Resources getResources() {
+        return getContext().getResources();
     }
 }
