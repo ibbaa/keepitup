c9962703a3adc943d8d47f23f64d32b47b3f0b05
Alwin <alwin.ibba@web.de>
2019-01-03 18:14:41 +0100

    List view running image

--- keepitupmain/src/main/java/de/ibba/keepitup/ui/adapter/NetworkTaskViewHolder.java
+++ keepitupmain/src/main/java/de/ibba/keepitup/ui/adapter/NetworkTaskViewHolder.java
@@ -3,6 +3,7 @@ package de.ibba.keepitup.ui.adapter;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import de.ibba.keepitup.R;
@@ -10,13 +11,16 @@ import de.ibba.keepitup.R;
 public class NetworkTaskViewHolder extends RecyclerView.ViewHolder {
 
     private final TextView statusText;
+    private final ImageView statusImage;
 
     public NetworkTaskViewHolder(@NonNull View itemView) {
         super(itemView);
         statusText = itemView.findViewById(R.id.textview_list_item_network_task_status);
+        statusImage = itemView.findViewById(R.id.image_list_item_network_task_status);
     }
 
-    public void setStatus(String status) {
+    public void setStatus(String status, int image) {
         statusText.setText(status);
+        statusImage.setImageResource(image);
     }
 }
