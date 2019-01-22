63290a33c9dee51b9cb76c7299a9974445f947db
Alwin Ibba <alwin@ibba.de>
2019-01-22 19:38:46 +0100

    add radio button programmatically

--- keepitupmain/src/main/java/de/ibba/keepitup/ui/NetworkTaskEditDialog.java
+++ keepitupmain/src/main/java/de/ibba/keepitup/ui/NetworkTaskEditDialog.java
@@ -8,6 +8,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import java.util.Objects;
 
@@ -24,6 +27,13 @@ public class NetworkTaskEditDialog extends DialogFragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.dialog_edit_network_task, container);
+        RadioGroup accessTypeGroup = view.findViewById(R.id.radiogroup_dialog_edit_network_task_accesstype);
+        RadioButton newRadioButton = new RadioButton(getContext());
+        newRadioButton.setText("Test");
+        newRadioButton.setId(View.generateViewId());
+        newRadioButton.setChecked(true);
+        LinearLayout.LayoutParams layoutParams = new RadioGroup.LayoutParams(RadioGroup.LayoutParams.WRAP_CONTENT, RadioGroup.LayoutParams.WRAP_CONTENT);
+        accessTypeGroup.addView(newRadioButton, 0, layoutParams);
         ImageView okImage = view.findViewById(R.id.imageview_dialog_edit_network_task_ok);
         ImageView cancelImage = view.findViewById(R.id.imageview_dialog_edit_network_task_cancel);
         okImage.setOnClickListener(this::onOkClicked);
