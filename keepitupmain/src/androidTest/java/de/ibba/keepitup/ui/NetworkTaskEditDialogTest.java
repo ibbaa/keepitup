62927e8b6077862c0b79afbca2a56a45b569b4ec
Alwin Ibba <alwin@ibba.de>
2019-02-23 13:52:32 +0100

    Error dialog test and small fixes in error dialog

--- keepitupmain/src/androidTest/java/de/ibba/keepitup/ui/NetworkTaskEditDialogTest.java
+++ keepitupmain/src/androidTest/java/de/ibba/keepitup/ui/NetworkTaskEditDialogTest.java
@@ -18,6 +18,7 @@ import de.ibba.keepitup.model.NetworkTask;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.hasChildCount;
 import static android.support.test.espresso.matcher.ViewMatchers.isChecked;
@@ -47,7 +48,7 @@ public class NetworkTaskEditDialogTest {
     }
 
     @Test
-    public void testAddNetworkTaskDefaultValues() {
+    public void testGetNetworkTaskDefaultValues() {
         ViewInteraction addNetworkTaskImageView = onView(allOf(withId(R.id.imageview_list_item_network_task_add), isDisplayed()));
         addNetworkTaskImageView.perform(click());
         onView(withId(R.id.radiogroup_dialog_edit_network_task_accesstype)).check(matches(hasChildCount(1)));
@@ -67,7 +68,7 @@ public class NetworkTaskEditDialogTest {
     }
 
     @Test
-    public void testAddNetworkTaskEnteredText() {
+    public void testGetNetworkTaskEnteredText() {
         ViewInteraction addNetworkTaskImageView = onView(allOf(withId(R.id.imageview_list_item_network_task_add), isDisplayed()));
         addNetworkTaskImageView.perform(click());
         onView(withId(R.id.edittext_dialog_edit_network_task_address)).perform(replaceText("localhost"));
@@ -82,5 +83,50 @@ public class NetworkTaskEditDialogTest {
         assertEquals(80, task.getPort());
         assertEquals(60, task.getInterval());
         assertTrue(task.isNotification());
+    }
+
+    @Test
+    public void testOnOkCancelClickedDialogDismissed() {
+        ViewInteraction addNetworkTaskImageView = onView(allOf(withId(R.id.imageview_list_item_network_task_add), isDisplayed()));
+        addNetworkTaskImageView.perform(click());
+        assertEquals(1, activity.getSupportFragmentManager().getFragments().size());
+        onView(withId(R.id.imageview_dialog_edit_network_task_cancel)).perform(click());
+        assertEquals(0, activity.getSupportFragmentManager().getFragments().size());
+        addNetworkTaskImageView.perform(click());
+        assertEquals(1, activity.getSupportFragmentManager().getFragments().size());
+        onView(withId(R.id.imageview_dialog_edit_network_task_ok)).perform(click());
+        assertEquals(0, activity.getSupportFragmentManager().getFragments().size());
+    }
+
+    @Test
+    public void testOnOkCancelClickedInputErrors() {
+        ViewInteraction addNetworkTaskImageView = onView(allOf(withId(R.id.imageview_list_item_network_task_add), isDisplayed()));
+        addNetworkTaskImageView.perform(click());
+        onView(withId(R.id.edittext_dialog_edit_network_task_address)).perform(replaceText("123.456"));
+        onView(withId(R.id.edittext_dialog_edit_network_task_port)).perform(replaceText("99999"));
+        onView(withId(R.id.edittext_dialog_edit_network_task_interval)).perform(replaceText("0"));
+        onView(withId(R.id.imageview_dialog_edit_network_task_ok)).perform(click());
+        assertEquals(2, activity.getSupportFragmentManager().getFragments().size());
+        onView(withText("Host")).check(matches(isDisplayed()));
+        onView(withText("No valid host or IP address")).check(matches(isDisplayed()));
+        onView(withText("Port")).check(matches(isDisplayed()));
+        onView(withText("Maximum: 65535")).check(matches(isDisplayed()));
+        onView(withText("Interval")).check(matches(isDisplayed()));
+        onView(withText("Minimum: 1")).check(matches(isDisplayed()));
+        onView(withId(R.id.imageview_dialog_edit_network_task_error_ok)).perform(click());
+        assertEquals(1, activity.getSupportFragmentManager().getFragments().size());
+        onView(withId(R.id.edittext_dialog_edit_network_task_port)).perform(replaceText("80"));
+        onView(withId(R.id.imageview_dialog_edit_network_task_ok)).perform(click());
+        assertEquals(2, activity.getSupportFragmentManager().getFragments().size());
+        onView(withText("Host")).check(matches(isDisplayed()));
+        onView(withText("No valid host or IP address")).check(matches(isDisplayed()));
+        onView(withText("Port")).check(doesNotExist());
+        onView(withText("Maximum: 65535")).check(doesNotExist());
+        onView(withText("Interval")).check(matches(isDisplayed()));
+        onView(withText("Minimum: 1")).check(matches(isDisplayed()));
+        onView(withId(R.id.imageview_dialog_edit_network_task_error_ok)).perform(click());
+        assertEquals(1, activity.getSupportFragmentManager().getFragments().size());
+        onView(withId(R.id.imageview_dialog_edit_network_task_cancel)).perform(click());
+        assertEquals(0, activity.getSupportFragmentManager().getFragments().size());
     }
 }
