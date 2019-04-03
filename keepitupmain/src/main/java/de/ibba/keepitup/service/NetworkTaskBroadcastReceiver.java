d7d754f49e18bd52e7898a4fbef19328656652c6
Alwin <alwin.ibba@web.de>
2019-04-03 20:16:51 +0200

    AlarmManager receiver implemented

--- keepitupmain/src/main/java/de/ibba/keepitup/service/NetworkTaskBroadcastReceiver.java
+++ keepitupmain/src/main/java/de/ibba/keepitup/service/NetworkTaskBroadcastReceiver.java
@@ -3,11 +3,24 @@ package de.ibba.keepitup.service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
+
+import java.util.Objects;
+
+import de.ibba.keepitup.model.NetworkTask;
 
 public class NetworkTaskBroadcastReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        NetworkTask task = new NetworkTask(Objects.requireNonNull(intent.getExtras()));
+        Log.d(NetworkTaskBroadcastReceiver.class.getName(), "Received request for " + task);
+        Log.d(NetworkTaskBroadcastReceiver.class.getName(), "Rescheduling " + task);
+        NetworkTaskServiceScheduler scheduler = new NetworkTaskServiceScheduler(context);
+        scheduler.reschedule(task, false);
+    }
 
+    private void doWork(NetworkTask task) {
+        Log.d(NetworkTaskBroadcastReceiver.class.getName(), "Doing work for " + task);
     }
 }
