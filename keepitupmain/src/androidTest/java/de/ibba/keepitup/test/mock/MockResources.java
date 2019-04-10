e34f122fe6a2c9c56ca7731609b7158ed5103afa
Alwin Ibba <alwin@ibba.de>
2019-04-10 20:40:56 +0200

    Mock implementation continued

--- keepitupmain/src/androidTest/java/de/ibba/keepitup/test/mock/MockResources.java
+++ keepitupmain/src/androidTest/java/de/ibba/keepitup/test/mock/MockResources.java
@@ -19,250 +19,264 @@ import org.xmlpull.v1.XmlPullParserException;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MockResources extends Resources {
 
     private final Resources testResources;
     private final Resources targetResources;
+    private final Map<Integer, String> testStringResources;
+    private final Map<Integer, String> targetStringResources;
 
     public MockResources(AssetManager assetManager, Resources testResources, Resources targetResources) {
         super(assetManager, null, null);
         this.testResources = testResources;
         this.targetResources = targetResources;
+        testStringResources = new HashMap<>();
+        targetStringResources = new HashMap<>();
+    }
+
+    private void parseStringResources(Class resources) {
+        Field[] fields = resources.getDeclaredFields();
+        for (Field currentField : fields) {
+
+        }
     }
 
     @Override
     public CharSequence getText(int id) throws NotFoundException {
-        return super.getText(id);
+        return targetResources.getText(id);
     }
 
     @Override
     public Typeface getFont(int id) throws NotFoundException {
-        return super.getFont(id);
+        return targetResources.getFont(id);
     }
 
     @Override
     public CharSequence getQuantityText(int id, int quantity) throws NotFoundException {
-        return super.getQuantityText(id, quantity);
+        return targetResources.getQuantityText(id, quantity);
     }
 
     @Override
     public String getString(int id) throws NotFoundException {
-        return super.getString(id);
+        return targetResources.getString(id);
     }
 
     @Override
     public String getString(int id, Object... formatArgs) throws NotFoundException {
-        return super.getString(id, formatArgs);
+        return targetResources.getString(id, formatArgs);
     }
 
     @Override
     public String getQuantityString(int id, int quantity, Object... formatArgs) throws NotFoundException {
-        return super.getQuantityString(id, quantity, formatArgs);
+        return targetResources.getQuantityString(id, quantity, formatArgs);
     }
 
     @Override
     public String getQuantityString(int id, int quantity) throws NotFoundException {
-        return super.getQuantityString(id, quantity);
+        return targetResources.getQuantityString(id, quantity);
     }
 
     @Override
     public CharSequence getText(int id, CharSequence def) {
-        return super.getText(id, def);
+        return targetResources.getText(id, def);
     }
 
     @Override
     public CharSequence[] getTextArray(int id) throws NotFoundException {
-        return super.getTextArray(id);
+        return targetResources.getTextArray(id);
     }
 
     @Override
     public String[] getStringArray(int id) throws NotFoundException {
-        return super.getStringArray(id);
+        return targetResources.getStringArray(id);
     }
 
     @Override
     public int[] getIntArray(int id) throws NotFoundException {
-        return super.getIntArray(id);
+        return targetResources.getIntArray(id);
     }
 
     @Override
     public TypedArray obtainTypedArray(int id) throws NotFoundException {
-        return super.obtainTypedArray(id);
+        return targetResources.obtainTypedArray(id);
     }
 
     @Override
     public float getDimension(int id) throws NotFoundException {
-        return super.getDimension(id);
+        return targetResources.getDimension(id);
     }
 
     @Override
     public int getDimensionPixelOffset(int id) throws NotFoundException {
-        return super.getDimensionPixelOffset(id);
+        return targetResources.getDimensionPixelOffset(id);
     }
 
     @Override
     public int getDimensionPixelSize(int id) throws NotFoundException {
-        return super.getDimensionPixelSize(id);
+        return targetResources.getDimensionPixelSize(id);
     }
 
     @Override
     public float getFraction(int id, int base, int pbase) {
-        return super.getFraction(id, base, pbase);
+        return targetResources.getFraction(id, base, pbase);
     }
 
     @Override
     public Drawable getDrawable(int id) throws NotFoundException {
-        return super.getDrawable(id);
+        return targetResources.getDrawable(id);
     }
 
     @Override
     public Drawable getDrawable(int id, Resources.Theme theme) throws NotFoundException {
-        return super.getDrawable(id, theme);
+        return targetResources.getDrawable(id, theme);
     }
 
     @Override
     public Drawable getDrawableForDensity(int id, int density) throws NotFoundException {
-        return super.getDrawableForDensity(id, density);
+        return targetResources.getDrawableForDensity(id, density);
     }
 
     @Override
     public Drawable getDrawableForDensity(int id, int density, Resources.Theme theme) {
-        return super.getDrawableForDensity(id, density, theme);
+        return targetResources.getDrawableForDensity(id, density, theme);
     }
 
     @Override
     public Movie getMovie(int id) throws NotFoundException {
-        return super.getMovie(id);
+        return targetResources.getMovie(id);
     }
 
     @Override
     public int getColor(int id) throws NotFoundException {
-        return super.getColor(id);
+        return targetResources.getColor(id);
     }
 
     @Override
     public int getColor(int id, Resources.Theme theme) throws NotFoundException {
-        return super.getColor(id, theme);
+        return targetResources.getColor(id, theme);
     }
 
     @Override
     public ColorStateList getColorStateList(int id) throws NotFoundException {
-        return super.getColorStateList(id);
+        return targetResources.getColorStateList(id);
     }
 
     @Override
     public ColorStateList getColorStateList(int id, Resources.Theme theme) throws NotFoundException {
-        return super.getColorStateList(id, theme);
+        return targetResources.getColorStateList(id, theme);
     }
 
     @Override
     public boolean getBoolean(int id) throws NotFoundException {
-        return super.getBoolean(id);
+        return targetResources.getBoolean(id);
     }
 
     @Override
     public int getInteger(int id) throws NotFoundException {
-        return super.getInteger(id);
+        return targetResources.getInteger(id);
     }
 
     @Override
     public XmlResourceParser getLayout(int id) throws NotFoundException {
-        return super.getLayout(id);
+        return targetResources.getLayout(id);
     }
 
     @Override
     public XmlResourceParser getAnimation(int id) throws NotFoundException {
-        return super.getAnimation(id);
+        return targetResources.getAnimation(id);
     }
 
     @Override
     public XmlResourceParser getXml(int id) throws NotFoundException {
-        return super.getXml(id);
+        return targetResources.getXml(id);
     }
 
     @Override
     public InputStream openRawResource(int id) throws NotFoundException {
-        return super.openRawResource(id);
+        return targetResources.openRawResource(id);
     }
 
     @Override
     public InputStream openRawResource(int id, TypedValue value) throws NotFoundException {
-        return super.openRawResource(id, value);
+        return targetResources.openRawResource(id, value);
     }
 
     @Override
     public AssetFileDescriptor openRawResourceFd(int id) throws NotFoundException {
-        return super.openRawResourceFd(id);
+        return targetResources.openRawResourceFd(id);
     }
 
     @Override
     public void getValue(int id, TypedValue outValue, boolean resolveRefs) throws NotFoundException {
-        super.getValue(id, outValue, resolveRefs);
+        targetResources.getValue(id, outValue, resolveRefs);
     }
 
     @Override
     public void getValueForDensity(int id, int density, TypedValue outValue, boolean resolveRefs) throws NotFoundException {
-        super.getValueForDensity(id, density, outValue, resolveRefs);
+        targetResources.getValueForDensity(id, density, outValue, resolveRefs);
     }
 
     @Override
     public void getValue(String name, TypedValue outValue, boolean resolveRefs) throws NotFoundException {
-        super.getValue(name, outValue, resolveRefs);
+        targetResources.getValue(name, outValue, resolveRefs);
     }
 
     @Override
     public TypedArray obtainAttributes(AttributeSet set, int[] attrs) {
-        return super.obtainAttributes(set, attrs);
+        return targetResources.obtainAttributes(set, attrs);
     }
 
     @Override
     public void updateConfiguration(Configuration config, DisplayMetrics metrics) {
-        super.updateConfiguration(config, metrics);
+        targetResources.updateConfiguration(config, metrics);
     }
 
     @Override
     public DisplayMetrics getDisplayMetrics() {
-        return super.getDisplayMetrics();
+        return targetResources.getDisplayMetrics();
     }
 
     @Override
     public Configuration getConfiguration() {
-        return super.getConfiguration();
+        return targetResources.getConfiguration();
     }
 
     @Override
     public int getIdentifier(String name, String defType, String defPackage) {
-        return super.getIdentifier(name, defType, defPackage);
+        return targetResources.getIdentifier(name, defType, defPackage);
     }
 
     @Override
     public String getResourceName(int resid) throws NotFoundException {
-        return super.getResourceName(resid);
+        return targetResources.getResourceName(resid);
     }
 
     @Override
     public String getResourcePackageName(int resid) throws NotFoundException {
-        return super.getResourcePackageName(resid);
+        return targetResources.getResourcePackageName(resid);
     }
 
     @Override
     public String getResourceTypeName(int resid) throws NotFoundException {
-        return super.getResourceTypeName(resid);
+        return targetResources.getResourceTypeName(resid);
     }
 
     @Override
     public String getResourceEntryName(int resid) throws NotFoundException {
-        return super.getResourceEntryName(resid);
+        return targetResources.getResourceEntryName(resid);
     }
 
     @Override
     public void parseBundleExtras(XmlResourceParser parser, Bundle outBundle) throws IOException, XmlPullParserException {
-        super.parseBundleExtras(parser, outBundle);
+        targetResources.parseBundleExtras(parser, outBundle);
     }
 
     @Override
     public void parseBundleExtra(String tagName, AttributeSet attrs, Bundle outBundle) throws XmlPullParserException {
-        super.parseBundleExtra(tagName, attrs, outBundle);
+        targetResources.parseBundleExtra(tagName, attrs, outBundle);
     }
 }
