b86dabf111317d17fa01887529b25813bd080e3e
Alwin Ibba <alwin@ibba.de>
2019-04-14 13:26:56 +0200

    improved NetworkTaskServiceSchedulerTest

--- keepitupmain/src/androidTest/java/de/ibba/keepitup/service/NetworkTaskServiceSchedulerTest.java
+++ keepitupmain/src/androidTest/java/de/ibba/keepitup/service/NetworkTaskServiceSchedulerTest.java
@@ -10,9 +10,12 @@ import org.junit.runner.RunWith;
 
 import de.ibba.keepitup.model.AccessType;
 import de.ibba.keepitup.model.NetworkTask;
+import de.ibba.keepitup.test.mock.MockAlarmManager;
 import de.ibba.keepitup.test.mock.TestRegistry;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
 @MediumTest
@@ -20,11 +23,13 @@ import static org.junit.Assert.assertTrue;
 public class NetworkTaskServiceSchedulerTest {
 
     private NetworkTaskServiceScheduler scheduler;
+    private MockAlarmManager alarmManager;
 
     @Before
     public void beforeEachTestMethod() {
         scheduler = new NetworkTaskServiceScheduler(TestRegistry.getContext());
         scheduler.cancelAll();
+        alarmManager = (MockAlarmManager) scheduler.getAlarmManager();
     }
 
     @After
@@ -39,22 +44,38 @@ public class NetworkTaskServiceSchedulerTest {
         task1 = scheduler.schedule(task1);
         assertTrue(task1.isRunning());
         assertFalse(task2.isRunning());
+        assertTrue(alarmManager.wasSetAlarmCalled());
+        assertFalse(alarmManager.wasCancelAlarmCalled());
+        MockAlarmManager.SetAlarmCall setAlarmCall1 = alarmManager.getSetAlarmCalls().get(0);
+        assertEquals(0, setAlarmCall1.getDelay());
         task2 = scheduler.schedule(task2);
         assertTrue(task1.isRunning());
         assertTrue(task2.isRunning());
+        assertTrue(alarmManager.wasSetAlarmCalled());
+        assertFalse(alarmManager.wasCancelAlarmCalled());
+        MockAlarmManager.SetAlarmCall setAlarmCall2 = alarmManager.getSetAlarmCalls().get(1);
+        assertEquals(0, setAlarmCall2.getDelay());
+        assertNotEquals(setAlarmCall1.getPendingIntent(), setAlarmCall2.getPendingIntent());
         task1 = scheduler.cancel(task1);
         assertFalse(task1.isRunning());
         assertTrue(task2.isRunning());
+        assertTrue(alarmManager.wasCancelAlarmCalled());
+        MockAlarmManager.CancelAlarmCall cancelAlarmCall1 = alarmManager.getCancelAlarmCalls().get(0);
+        assertEquals(setAlarmCall1.getPendingIntent(), cancelAlarmCall1.getPendingIntent());
         task2 = scheduler.cancel(task2);
         assertFalse(task1.isRunning());
         assertFalse(task2.isRunning());
+        assertTrue(alarmManager.wasCancelAlarmCalled());
+        MockAlarmManager.CancelAlarmCall cancelAlarmCall2 = alarmManager.getCancelAlarmCalls().get(1);
+        assertEquals(setAlarmCall2.getPendingIntent(), cancelAlarmCall2.getPendingIntent());
+        assertNotEquals(cancelAlarmCall1.getPendingIntent(), cancelAlarmCall2.getPendingIntent());
     }
 
     private NetworkTask getNetworkTask1() {
         NetworkTask task = new NetworkTask();
         task.setId(1);
         task.setIndex(1);
-        task.setSchedulerId(0);
+        task.setSchedulerId(1);
         task.setAddress("127.0.0.1");
         task.setPort(80);
         task.setAccessType(AccessType.PING);
@@ -68,7 +89,7 @@ public class NetworkTaskServiceSchedulerTest {
         NetworkTask task = new NetworkTask();
         task.setId(2);
         task.setIndex(10);
-        task.setSchedulerId(0);
+        task.setSchedulerId(2);
         task.setAddress("host.com");
         task.setPort(21);
         task.setAccessType(null);
