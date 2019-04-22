68fe122f287a795f2d1fcad918c20c2ba54ffe56
Alwin <alwin.ibba@web.de>
2019-04-22 12:33:01 +0200

    UI sync test continued, test not running yet, must debug

--- keepitupmain/src/androidTest/java/de/ibba/keepitup/ui/sync/UISyncControllerTest.java
+++ keepitupmain/src/androidTest/java/de/ibba/keepitup/ui/sync/UISyncControllerTest.java
@@ -8,10 +8,18 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+import de.ibba.keepitup.model.AccessType;
+import de.ibba.keepitup.model.LogEntry;
+import de.ibba.keepitup.model.NetworkTask;
 import de.ibba.keepitup.test.mock.MockHandler;
 import de.ibba.keepitup.ui.BaseUITest;
 import de.ibba.keepitup.ui.NetworkTaskMainActivity;
 import de.ibba.keepitup.ui.adapter.NetworkTaskAdapter;
+import de.ibba.keepitup.ui.adapter.NetworkTaskUIWrapper;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -26,7 +34,6 @@ public class UISyncControllerTest extends BaseUITest {
     @Rule
     public final ActivityTestRule<NetworkTaskMainActivity> rule = new ActivityTestRule<>(NetworkTaskMainActivity.class, false, false);
 
-    @Test
     public void testStartStop() {
         launchRecyclerViewBaseActivity(rule);
         assertTrue(UISyncController.isRunning());
@@ -45,6 +52,93 @@ public class UISyncControllerTest extends BaseUITest {
         MockHandler.StopCall stopCall = handler.getStopCalls().get(0);
         Runnable stoppedRunnable = stopCall.getRunnable();
         assertSame(startedRunnable, stoppedRunnable);
+    }
+
+    @Test
+    public void testUpdateAdapter() {
+        NetworkTask task1 = getNetworkTask1();
+        NetworkTask task2 = getNetworkTask2();
+        NetworkTask task3 = getNetworkTask3();
+        task1 = getNetworkTaskDAO().insertNetworkTask(task1);
+        task2 = getNetworkTaskDAO().insertNetworkTask(task2);
+        task3 = getNetworkTaskDAO().insertNetworkTask(task3);
+        LogEntry entry11 = getLogEntry(task1, new GregorianCalendar(2016, Calendar.JULY, 1), true, "Message11");
+        LogEntry entry21 = getLogEntry(task2, new GregorianCalendar(2016, Calendar.JULY, 1), true, "Message21");
+        LogEntry entry31 = getLogEntry(task3, new GregorianCalendar(2016, Calendar.JULY, 1), true, "Message31");
+        getLogDAO().insertAndDeleteLog(entry11);
+        getLogDAO().insertAndDeleteLog(entry21);
+        getLogDAO().insertAndDeleteLog(entry31);
+        NetworkTaskMainActivity activity = (NetworkTaskMainActivity) launchRecyclerViewBaseActivity(rule);
+        UISyncController.stop();
+        LogEntry entry12 = getLogEntry(task1, new GregorianCalendar(2016, Calendar.JULY, 2), true, "Message12");
+        LogEntry entry22 = getLogEntry(task2, new GregorianCalendar(2016, Calendar.JULY, 2), true, "Message22");
+        LogEntry entry32 = getLogEntry(task3, new GregorianCalendar(2016, Calendar.JULY, 2), true, "Message32");
+        getLogDAO().insertAndDeleteLog(entry12);
+        getLogDAO().insertAndDeleteLog(entry22);
+        getLogDAO().insertAndDeleteLog(entry32);
+        activity.runOnUiThread(() -> UISyncController.start(getAdapter()));
+        List<NetworkTaskUIWrapper> uiWWrapperList = getAdapter().getAllItems();
+        LogEntry adapterEntry1 = uiWWrapperList.get(0).getLogEntry();
+        LogEntry adapterEntry2 = uiWWrapperList.get(1).getLogEntry();
+        LogEntry adapterEntry3 = uiWWrapperList.get(2).getLogEntry();
+        assertEquals("Message11", adapterEntry1.getMessage());
+        assertEquals("Message22", adapterEntry2.getMessage());
+        assertEquals("Message31", adapterEntry3.getMessage());
+    }
+
+    private NetworkTask getNetworkTask1() {
+        NetworkTask networkTask = new NetworkTask();
+        networkTask.setId(-1);
+        networkTask.setIndex(0);
+        networkTask.setSchedulerId(-1);
+        networkTask.setAddress("127.0.0.1");
+        networkTask.setPort(80);
+        networkTask.setAccessType(AccessType.PING);
+        networkTask.setInterval(15);
+        networkTask.setOnlyWifi(false);
+        networkTask.setNotification(true);
+        networkTask.setRunning(false);
+        return networkTask;
+    }
+
+    private NetworkTask getNetworkTask2() {
+        NetworkTask networkTask = new NetworkTask();
+        networkTask.setId(-1);
+        networkTask.setIndex(1);
+        networkTask.setSchedulerId(-1);
+        networkTask.setAddress("127.0.0.1");
+        networkTask.setPort(80);
+        networkTask.setAccessType(AccessType.PING);
+        networkTask.setInterval(15);
+        networkTask.setOnlyWifi(false);
+        networkTask.setNotification(true);
+        networkTask.setRunning(true);
+        return networkTask;
+    }
+
+    private NetworkTask getNetworkTask3() {
+        NetworkTask networkTask = new NetworkTask();
+        networkTask.setId(-1);
+        networkTask.setIndex(2);
+        networkTask.setSchedulerId(-1);
+        networkTask.setAddress("127.0.0.1");
+        networkTask.setPort(80);
+        networkTask.setAccessType(AccessType.PING);
+        networkTask.setInterval(15);
+        networkTask.setOnlyWifi(false);
+        networkTask.setNotification(true);
+        networkTask.setRunning(false);
+        return networkTask;
+    }
+
+    private LogEntry getLogEntry(NetworkTask task, Calendar calendar, boolean success, String message) {
+        LogEntry logEntry = new LogEntry();
+        logEntry.setId(0);
+        logEntry.setNetworkTaskId(task.getId());
+        logEntry.setSuccess(success);
+        logEntry.setTimestamp(calendar.getTime().getTime());
+        logEntry.setMessage(message);
+        return logEntry;
     }
 
     private NetworkTaskAdapter getAdapter() {
