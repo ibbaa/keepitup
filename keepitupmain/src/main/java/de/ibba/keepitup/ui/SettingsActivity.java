bf80095920f605752df1f188868f8d33d65d6674
Alwin Ibba <alwin@ibba.de>
2019-05-12 11:00:45 +0200

    SettingsInputDialog prepared and some refactorings

--- keepitupmain/src/main/java/de/ibba/keepitup/ui/SettingsActivity.java
+++ keepitupmain/src/main/java/de/ibba/keepitup/ui/SettingsActivity.java
@@ -23,7 +23,7 @@ import de.ibba.keepitup.util.BundleUtil;
 import de.ibba.keepitup.util.NumberUtil;
 import de.ibba.keepitup.util.StringUtil;
 
-public class NetworkTaskSettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity {
 
     public static final int SETTING_ACTIVITY_CODE = 1000;
 
@@ -45,7 +45,7 @@ public class NetworkTaskSettingsActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == R.id.menu_action_reset) {
-            Log.d(NetworkTaskSettingsActivity.class.getName(), "menu_action_defaults triggered");
+            Log.d(SettingsActivity.class.getName(), "menu_action_defaults triggered");
             SharedPreferences.Editor preferencesEditor = PreferenceManager.getDefaultSharedPreferences(this).edit();
             preferencesEditor.remove(getResources().getString(R.string.interval_setting_key));
             preferencesEditor.remove(getResources().getString(R.string.key_settings_defaults_address));
@@ -70,44 +70,44 @@ public class NetworkTaskSettingsActivity extends AppCompatActivity {
 
         @SuppressWarnings("unused")
         boolean onIntervalChanged(Preference preference, Object newValue) {
-            Log.d(NetworkTaskSettingsActivity.class.getName(), "onIntervalChanged validating input " + newValue);
+            Log.d(SettingsActivity.class.getName(), "onIntervalChanged validating input " + newValue);
             boolean isValidValue = NumberUtil.isValidLongValue(newValue);
             long refreshInterval = NumberUtil.getLongValue(newValue, getResources().getInteger(R.integer.interval_setting_default));
             long refreshIntervalMinimum = getResources().getInteger(R.integer.interval_setting_minimum);
             if (!isValidValue || refreshInterval < refreshIntervalMinimum) {
-                Log.d(NetworkTaskSettingsActivity.class.getName(), "onIntervalChanged, input " + newValue + " is invalid");
+                Log.d(SettingsActivity.class.getName(), "onIntervalChanged, input " + newValue + " is invalid");
                 String failure = getResources().getString(R.string.interval_setting_label) + System.lineSeparator()
                         + getResources().getString(R.string.text_alert_dialog_value) + ": " + newValue + System.lineSeparator()
                         + getResources().getString(R.string.text_alert_dialog_minimum) + ": " + refreshIntervalMinimum;
                 showErrorDialog(failure);
                 return false;
             }
-            Log.d(NetworkTaskSettingsActivity.class.getName(), "onIntervalChanged, input " + newValue + " is valid");
+            Log.d(SettingsActivity.class.getName(), "onIntervalChanged, input " + newValue + " is valid");
             return true;
         }
 
         @SuppressWarnings("unused")
         boolean onAddressChanged(Preference preference, Object newValue) {
-            Log.d(NetworkTaskSettingsActivity.class.getName(), "onAddressChanged validating input " + newValue);
+            Log.d(SettingsActivity.class.getName(), "onAddressChanged validating input " + newValue);
             String address = StringUtil.getStringValue(newValue, "");
             Validator hostValidator = new StandardHostPortValidator(getActivity());
             Validator urlValidator = new URLValidator(getActivity());
             ValidationResult validHostResult = hostValidator.validateAddress(address);
             ValidationResult validURLResult = urlValidator.validateAddress(address);
             if (!validHostResult.isValidationSuccessful() && !validURLResult.isValidationSuccessful()) {
-                Log.d(NetworkTaskSettingsActivity.class.getName(), "onHostnameChanged, input " + newValue + " is invalid");
+                Log.d(SettingsActivity.class.getName(), "onHostnameChanged, input " + newValue + " is invalid");
                 Bundle bundle = new Bundle();
                 BundleUtil.addValidationResultToIndexedBundle(bundle, validHostResult);
                 BundleUtil.addValidationResultToIndexedBundle(bundle, validURLResult);
                 showErrorDialog(bundle);
                 return false;
             }
-            Log.d(NetworkTaskSettingsActivity.class.getName(), "onHostnameChanged, input " + newValue + " is valid");
+            Log.d(SettingsActivity.class.getName(), "onHostnameChanged, input " + newValue + " is valid");
             return true;
         }
 
         private void showErrorDialog(Bundle bundle) {
-            Log.d(NetworkTaskSettingsActivity.class.getName(), "showErrorDialog, opening NetworkTaskValidatorErrorDialog");
+            Log.d(SettingsActivity.class.getName(), "showErrorDialog, opening NetworkTaskValidatorErrorDialog");
             NetworkTaskValidatorErrorDialog errorDialog = new NetworkTaskValidatorErrorDialog();
             errorDialog.setArguments(bundle);
             errorDialog.show(Objects.requireNonNull(getFragmentManager()), NetworkTaskValidatorErrorDialog.class.getName());
