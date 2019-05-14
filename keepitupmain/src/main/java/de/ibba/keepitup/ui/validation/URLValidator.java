f39e8e6b1dd1b409b78412186b0ae190ac44e691
Alwin Ibba <alwin@ibba.de>
2019-05-14 19:51:33 +0200

    Field validators continued, validators refactored to use field validators

--- keepitupmain/src/main/java/de/ibba/keepitup/ui/validation/URLValidator.java
+++ keepitupmain/src/main/java/de/ibba/keepitup/ui/validation/URLValidator.java
@@ -4,8 +4,6 @@ import android.content.Context;
 import android.util.Log;
 
 import de.ibba.keepitup.R;
-import de.ibba.keepitup.util.StringUtil;
-import de.ibba.keepitup.util.URLUtil;
 
 public class URLValidator extends BaseValidator implements Validator {
 
@@ -17,20 +15,8 @@ public class URLValidator extends BaseValidator implements Validator {
     public ValidationResult validateAddress(String address) {
         Log.d(URLValidator.class.getName(), "validateAddress, address is " + address);
         String fieldName = getResources().getString(R.string.task_url_field_name);
-        String successMessage = getResources().getString(R.string.validation_successful);
-        String failedMessage = getResources().getString(R.string.invalid_url_format);
-        if (StringUtil.isEmpty(address)) {
-            Log.d(URLValidator.class.getName(), "No value specified. Validation failed.");
-            return new ValidationResult(false, fieldName, failedMessage);
-        }
-        Log.d(URLValidator.class.getName(), "Encoding and modifying URL.");
-        String encodedURL = URLUtil.encodeURL(address);
-        Log.d(URLValidator.class.getName(), "Modified URL is " + encodedURL);
-        if (URLUtil.isValidURL(encodedURL)) {
-            Log.d(URLValidator.class.getName(), "Valid URL. Validation successful.");
-            return new ValidationResult(true, fieldName, successMessage);
-        }
-        Log.d(URLValidator.class.getName(), "Invalid URL. Validation failed.");
-        return new ValidationResult(false, fieldName, failedMessage);
+        ValidationResult result = new URLFieldValidator(fieldName, getContext()).validate(address);
+        Log.d(URLValidator.class.getName(), URLFieldValidator.class.getSimpleName() + " returned " + result);
+        return result;
     }
 }
