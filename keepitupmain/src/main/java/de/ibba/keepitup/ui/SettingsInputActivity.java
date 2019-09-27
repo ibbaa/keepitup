6d2c504eebe1a2dad75a36f424dda1d0f90ded31
Alwin Ibba <alwin@ibba.de>
2019-09-27 19:48:14 +0200

    Bug fix reset settings values, switch settings

--- keepitupmain/src/main/java/de/ibba/keepitup/ui/SettingsInputActivity.java
+++ keepitupmain/src/main/java/de/ibba/keepitup/ui/SettingsInputActivity.java
@@ -1,11 +1,23 @@
 package de.ibba.keepitup.ui;
 
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import de.ibba.keepitup.ui.dialog.SettingsInput;
 import de.ibba.keepitup.ui.dialog.SettingsInputDialog;
 
-public interface SettingsInputActivity {
+public abstract class SettingsInputActivity extends AppCompatActivity {
 
-    void onInputDialogOkClicked(SettingsInputDialog inputDialog, SettingsInput.Type type);
+    public abstract void onInputDialogOkClicked(SettingsInputDialog inputDialog, SettingsInput.Type type);
 
-    void onInputDialogCancelClicked(SettingsInputDialog inputDialog);
+    public abstract void onInputDialogCancelClicked(SettingsInputDialog inputDialog);
+
+    protected void recreateActivity() {
+        Log.d(SettingsInputActivity.class.getName(), "recreateActivity");
+        finish();
+        overridePendingTransition(0, 0);
+        startActivity(getIntent());
+        overridePendingTransition(0, 0);
+    }
 }
