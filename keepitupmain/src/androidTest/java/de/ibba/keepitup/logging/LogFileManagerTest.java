8248701827a29879e7f6c86c8a7e10e22e56a822
Alwin Ibba <alwin@ibba.de>
2019-12-14 14:22:41 +0100

    Logging implementation continued

---
+++ keepitupmain/src/androidTest/java/de/ibba/keepitup/logging/LogFileManagerTest.java
@@ -0,0 +1,124 @@
+package de.ibba.keepitup.logging;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.SmallTest;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.File;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import de.ibba.keepitup.test.mock.TestRegistry;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+@SmallTest
+@RunWith(AndroidJUnit4.class)
+public class LogFileManagerTest {
+
+    private LogFileManager logFileManager;
+
+    @Before
+    public void beforeEachTestMethod() {
+        logFileManager = new LogFileManager();
+        logFileManager.delete(getTestLogFileFolder());
+    }
+
+    @Test
+    public void testGetFileNameExtension() {
+        assertEquals("", logFileManager.getFileNameExtension(null));
+        assertEquals("", logFileManager.getFileNameExtension(""));
+        assertEquals("jpg", logFileManager.getFileNameExtension("image.jpg"));
+        assertEquals("jpg", logFileManager.getFileNameExtension(".jpg"));
+        assertEquals("", logFileManager.getFileNameExtension("test"));
+        assertEquals("mp3", logFileManager.getFileNameExtension("test.wav.mp3"));
+    }
+
+    @Test
+    public void testGetFileNameWithoutExtension() {
+        assertEquals("", logFileManager.getFileNameWithoutExtension(null));
+        assertEquals("", logFileManager.getFileNameWithoutExtension(""));
+        assertEquals("image", logFileManager.getFileNameWithoutExtension("image.jpg"));
+        assertEquals("", logFileManager.getFileNameWithoutExtension(".jpg"));
+        assertEquals("test", logFileManager.getFileNameWithoutExtension("test"));
+        assertEquals("test.wav", logFileManager.getFileNameWithoutExtension("test.wav.mp3"));
+    }
+
+    @Test
+    public void testSuffixFileName() {
+        assertEquals("", logFileManager.suffixFileName(null, "abc"));
+        assertEquals("", logFileManager.suffixFileName("", "abc"));
+        assertEquals("", logFileManager.suffixFileName("", null));
+        assertEquals("", logFileManager.suffixFileName("", ""));
+        assertEquals("test", logFileManager.suffixFileName("test", null));
+        assertEquals("test", logFileManager.suffixFileName("test", ""));
+        assertEquals("test.jpg", logFileManager.suffixFileName("test.jpg", null));
+        assertEquals("test.jpg", logFileManager.suffixFileName("test.jpg", ""));
+        assertEquals("test_test.jpg", logFileManager.suffixFileName("test.jpg", "test"));
+        assertEquals("_test.jpg", logFileManager.suffixFileName(".jpg", "test"));
+        assertEquals("test.wav_123.mp3", logFileManager.suffixFileName("test.wav.mp3", "123"));
+        assertEquals("test.._test.jpg", logFileManager.suffixFileName("test...jpg", "test"));
+        assertEquals("test_abc", logFileManager.suffixFileName("test", "abc"));
+    }
+
+    @Test
+    public void testDeleteDirectory() throws Exception {
+        File logDir = getTestLogFileFolder();
+        assertEquals(0, logDir.listFiles().length);
+        File testFile = new File(logDir, "testfile");
+        assertTrue(testFile.createNewFile());
+        File testDirectory = new File(logDir, "testdirectory");
+        assertTrue(testDirectory.mkdir());
+        assertEquals(2, logDir.listFiles().length);
+        logFileManager.delete(logDir);
+        assertFalse(logDir.exists());
+    }
+
+    @Test
+    public void testGetValidFileName() throws Exception {
+        File logDir = getTestLogFileFolder();
+        long testTimestamp = getTestTimestamp();
+        String fileName = logFileManager.getValidFileName(logDir, "test.file", testTimestamp);
+        assertEquals("test.file", fileName);
+        File file = new File(logDir.getAbsolutePath(), fileName);
+        assertTrue(file.createNewFile());
+        fileName = logFileManager.getValidFileName(logDir, "test.file", testTimestamp);
+        assertEquals("test_1985.12.24_01_01_01.999.file", fileName);
+        file = new File(logDir.getAbsolutePath(), fileName);
+        assertTrue(file.createNewFile());
+        fileName = logFileManager.getValidFileName(logDir, "test.file", testTimestamp);
+        assertEquals("test_1985.12.24_01_01_01.999_(1).file", fileName);
+        file = new File(logDir.getAbsolutePath(), fileName);
+        assertTrue(file.createNewFile());
+        fileName = logFileManager.getValidFileName(logDir, "test.file", testTimestamp);
+        assertEquals("test_1985.12.24_01_01_01.999_(2).file", fileName);
+        file = new File(logDir, "test_1985.12.24_01_01_01.999.file");
+        assertTrue(file.delete());
+        fileName = logFileManager.getValidFileName(logDir, "test.file", testTimestamp);
+        assertEquals("test_1985.12.24_01_01_01.999.file", fileName);
+        file = new File(logDir.getAbsolutePath(), "test.file");
+        assertTrue(file.delete());
+        fileName = logFileManager.getValidFileName(logDir, "test.file", testTimestamp);
+        assertEquals("test.file", fileName);
+    }
+
+    private File getTestLogFileFolder() {
+        File dir = TestRegistry.getContext().getFilesDir();
+        File logDir = new File(dir, "logdir");
+        if (!logDir.exists()) {
+            assertTrue(logDir.mkdirs());
+        }
+        return logDir;
+    }
+
+    private long getTestTimestamp() {
+        Calendar calendar = new GregorianCalendar(1985, Calendar.DECEMBER, 24, 1, 1, 1);
+        calendar.set(Calendar.MILLISECOND, 999);
+        return calendar.getTimeInMillis();
+    }
+}
---
+++ keepitupmain/src/main/java/de/ibba/keepitup/logging/LogFileManager.java
@@ -0,0 +1,97 @@
+package de.ibba.keepitup.logging;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import de.ibba.keepitup.util.FileUtil;
+import de.ibba.keepitup.util.StringUtil;
+
+public class LogFileManager {
+
+    private final static int MAX_DUPLICATE_FILES = 99;
+    private final static String SUFFIX_FILE_PATTERN = "yyyy.MM.dd_HH_mm_ss.SSS";
+
+    public String getFileNameExtension(String fileName) {
+        if (fileName == null) {
+            return "";
+        }
+        int extensionIndex = fileName.lastIndexOf('.');
+        return extensionIndex < 0 ? "" : fileName.substring(extensionIndex + 1);
+    }
+
+    public String getFileNameWithoutExtension(String fileName) {
+        if (fileName == null) {
+            return "";
+        }
+        int extensionIndex = fileName.lastIndexOf('.');
+        return fileName.substring(0, extensionIndex < 0 ? fileName.length() : extensionIndex);
+    }
+
+    public String suffixFileName(String fileName, String suffix) {
+        if (fileName == null || fileName.isEmpty()) {
+            return "";
+        }
+        if (suffix == null || suffix.isEmpty()) {
+            return fileName;
+        }
+        String extension = getFileNameExtension(fileName);
+        if (!StringUtil.isEmpty(extension)) {
+            return getFileNameWithoutExtension(fileName) + "_" + suffix + "." + extension;
+        }
+        return getFileNameWithoutExtension(fileName) + "_" + suffix;
+    }
+
+    public boolean delete(File file) {
+        try {
+            File[] files = file.listFiles();
+            if (files != null) {
+                for (File currentFile : files) {
+                    delete(currentFile);
+                }
+            }
+            return file.delete();
+        } catch (Exception exc) {
+            return false;
+        }
+    }
+
+    public String getValidFileName(File folder, String file, long timestamp) {
+        try {
+            if (!folder.exists()) {
+                if (!folder.mkdirs()) {
+                    return null;
+                }
+            }
+            File resultingFile = new File(folder, file);
+            if (!resultingFile.exists()) {
+                return file;
+            }
+            String timestampFileName = suffixFileName(file, getTimestampSuffix(timestamp));
+            resultingFile = new File(folder, timestampFileName);
+            if (!resultingFile.exists()) {
+                return timestampFileName;
+            }
+            for (int ii = 1; ii <= MAX_DUPLICATE_FILES; ii++) {
+                String numberFileName = FileUtil.suffixFileName(timestampFileName, getNumberSuffix(ii));
+                resultingFile = new File(folder, numberFileName);
+                if (!resultingFile.exists()) {
+                    return numberFileName;
+                }
+            }
+        } catch (Exception exc) {
+            //Do nothing
+        }
+        return null;
+    }
+
+    private String getTimestampSuffix(long timestamp) {
+        SimpleDateFormat fileNameDateFormat = new SimpleDateFormat(SUFFIX_FILE_PATTERN, Locale.US);
+        return fileNameDateFormat.format(new Date(timestamp));
+    }
+
+    private String getNumberSuffix(int number) {
+        return "(" + number + ")";
+    }
+}
