7f27db2c86051202eb3094275c26844027afd7c1
Alwin Ibba <alwin@ibba.de>
2020-01-30 20:03:05 +0100

    implemented formatTimeRange method

--- keepitupmain/src/androidTest/java/de/ibba/keepitup/util/StringUtilTest.java
+++ keepitupmain/src/androidTest/java/de/ibba/keepitup/util/StringUtilTest.java
@@ -2,9 +2,14 @@ package de.ibba.keepitup.util;
 
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.SmallTest;
+import androidx.test.platform.app.InstrumentationRegistry;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.Locale;
+
+import de.ibba.keepitup.test.mock.TestRegistry;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -44,5 +49,20 @@ public class StringUtilTest {
         assertEquals("123", StringUtil.notNull("123"));
         assertEquals("", StringUtil.notNull(""));
         assertEquals("", StringUtil.notNull(null));
+    }
+
+    @Test
+    public void testFormatTimeRange() {
+        InstrumentationRegistry.getInstrumentation().getTargetContext().getResources().getConfiguration().setLocale(Locale.US);
+        assertEquals("123 msec", StringUtil.formatTimeRange(123, TestRegistry.getContext()));
+        assertEquals("0 msec", StringUtil.formatTimeRange(0, TestRegistry.getContext()));
+        assertEquals("0 msec", StringUtil.formatTimeRange(0.0, TestRegistry.getContext()));
+        assertEquals("0 msec", StringUtil.formatTimeRange(0.001, TestRegistry.getContext()));
+        assertEquals("0.01 msec", StringUtil.formatTimeRange(0.01, TestRegistry.getContext()));
+        assertEquals("1 sec", StringUtil.formatTimeRange(1000, TestRegistry.getContext()));
+        assertEquals("1,000 sec", StringUtil.formatTimeRange(1000000, TestRegistry.getContext()));
+        assertEquals("12.34 sec", StringUtil.formatTimeRange(12345, TestRegistry.getContext()));
+        assertEquals("5.55 sec", StringUtil.formatTimeRange(5550, TestRegistry.getContext()));
+        assertEquals("12.57 msec", StringUtil.formatTimeRange(12.5678999999, TestRegistry.getContext()));
     }
 }
