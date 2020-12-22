8f18714363d05a49f038c69df5dafa8731e1ed4e
alwin <alwin@ibba.de>
2020-12-22 12:40:32 +0100

    Reset config implementation started

--- keepitupmain/src/main/java/de/ibba/keepitup/ui/SettingsInputActivity.java
+++ keepitupmain/src/main/java/de/ibba/keepitup/ui/SettingsInputActivity.java
@@ -12,13 +12,14 @@ import de.ibba.keepitup.service.IPowerManager;
 import de.ibba.keepitup.service.SystemFileManager;
 import de.ibba.keepitup.service.SystemPowerManager;
 import de.ibba.keepitup.ui.dialog.BatteryOptimizationDialog;
+import de.ibba.keepitup.ui.dialog.ConfirmDialog;
 import de.ibba.keepitup.ui.dialog.FileChooseDialog;
 import de.ibba.keepitup.ui.dialog.GeneralErrorDialog;
 import de.ibba.keepitup.ui.dialog.SettingsInput;
 import de.ibba.keepitup.ui.dialog.SettingsInputDialog;
 import de.ibba.keepitup.util.BundleUtil;
 
-public abstract class SettingsInputActivity extends AppCompatActivity implements SettingsInputSupport, FileChooseSupport, BatteryOptimizationSupport {
+public abstract class SettingsInputActivity extends AppCompatActivity implements SettingsInputSupport, FileChooseSupport, BatteryOptimizationSupport, ConfirmSupport {
 
     private Resources resources;
     private IFileManager fileManager;
@@ -60,21 +61,25 @@ public abstract class SettingsInputActivity extends AppCompatActivity implements
         return new SystemPowerManager(this);
     }
 
+    @Override
     public void onInputDialogOkClicked(SettingsInputDialog inputDialog, SettingsInput.Type type) {
         Log.d(SettingsInputActivity.class.getName(), "onInputDialogOkClicked, type is " + type + ", value is " + inputDialog.getValue());
         inputDialog.dismiss();
     }
 
+    @Override
     public void onInputDialogCancelClicked(SettingsInputDialog inputDialog) {
         Log.d(SettingsInputActivity.class.getName(), "onInputDialogCancelClicked");
         inputDialog.dismiss();
     }
 
+    @Override
     public void onFileChooseDialogOkClicked(FileChooseDialog chooseDialog, FileChooseDialog.Type type) {
         Log.d(SettingsInputActivity.class.getName(), "onFolderChooseDialogOkClicked");
         chooseDialog.dismiss();
     }
 
+    @Override
     public void onFileChooseDialogCancelClicked(FileChooseDialog chooseDialog) {
         Log.d(SettingsInputActivity.class.getName(), "onFolderChooseDialogOkClicked");
         chooseDialog.dismiss();
@@ -84,6 +89,18 @@ public abstract class SettingsInputActivity extends AppCompatActivity implements
     public void onBatteryOptimizationDialogOkClicked(BatteryOptimizationDialog batteryOptimizationDialog) {
         Log.d(SettingsInputActivity.class.getName(), "onBatteryOptimizationDialogOkClicked");
         batteryOptimizationDialog.dismiss();
+    }
+
+    @Override
+    public void onConfirmDialogOkClicked(ConfirmDialog confirmDialog, ConfirmDialog.Type type) {
+        Log.d(SettingsInputActivity.class.getName(), "onConfirmDialogOkClicked");
+        confirmDialog.dismiss();
+    }
+
+    @Override
+    public void onConfirmDialogCancelClicked(ConfirmDialog confirmDialog) {
+        Log.d(SettingsInputActivity.class.getName(), "onConfirmDialogCancelClicked");
+        confirmDialog.dismiss();
     }
 
     protected void recreateActivity() {
@@ -105,5 +122,13 @@ public abstract class SettingsInputActivity extends AppCompatActivity implements
         bundle.putInt(errorDialog.getTypefaceStyleKey(), typeface);
         errorDialog.setArguments(bundle);
         errorDialog.show(getSupportFragmentManager(), GeneralErrorDialog.class.getName());
+    }
+
+    protected void showConfirmDialog(String confirmMessage, String description, ConfirmDialog.Type type) {
+        Log.d(SettingsInputActivity.class.getName(), "showConfirmDialog with message " + confirmMessage + " an description " + description + " for type " + type);
+        ConfirmDialog confirmDialog = new ConfirmDialog();
+        Bundle bundle = BundleUtil.stringsToBundle(new String[]{confirmDialog.getMessageKey(), confirmDialog.getDescriptionKey(), confirmDialog.getTypeKey()}, new String[]{confirmMessage, description, type.name()});
+        confirmDialog.setArguments(bundle);
+        confirmDialog.show(getSupportFragmentManager(), ConfirmDialog.class.getName());
     }
 }
