1d504f80be8bc20e136aaab9d9d5748f18eea777
Alwin Ibba <alwin@ibba.de>
2021-08-31 18:14:52 +0200

    renamed signing data environment variables

--- docker_build.sh
+++ docker_build.sh
@@ -17,7 +17,7 @@
 
 UID=$(id -u)
 GID=$(id -g)
-docker build --no-cache --build-arg UID=$UID --build-arg GID=$GID --build-arg KEEPITUP_KEY_FILE=$KEEPITUP_KEY_FILE --build-arg KEEPITUP_SIGNING_FILE=$KEEPITUP_SIGNING_FILE -t ibbaa/keepitup:build .
+docker build --no-cache --build-arg UID=$UID --build-arg GID=$GID --build-arg KEEPITUP_KEYSTORE=$KEEPITUP_KEYSTORE --build-arg KEEPITUP_SIGNING_PROPERTIES=$KEEPITUP_SIGNING_PROPERTIES -t ibbaa/keepitup:build .
 docker run $DOCKER_OPTS --rm -v $PWD:/var/keepitup -w /var/keepitup ibbaa/keepitup:build /bin/bash -c "./gradlew assemble"
 RET=$?
 docker image prune -f
