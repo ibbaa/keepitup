7d33944bdc71c8a1efcb3e05a6c76505aab9ce07
Alwin Ibba <alwin@ibba.de>
2021-09-03 13:21:51 +0200

    pass signing env variables to docker

--- docker_build.sh
+++ docker_build.sh
@@ -17,7 +17,7 @@
 
 UID=$(id -u)
 GID=$(id -g)
-docker build --no-cache --build-arg UID=$UID --build-arg GID=$GID -t ibbaa/keepitup:build .
+docker build --no-cache --build-arg UID=$UID --build-arg GID=$GID --build-arg KEEPITUP_KEYSTORE_FILE=$KEEPITUP_KEYSTORE_FILE --build-arg KEEPITUP_KEY_ALIAS=$KEEPITUP_KEY_ALIAS --build-arg KEEPITUP_KEYSTORE_PASS=$KEEPITUP_KEYSTORE_PASS --build-arg KEEPITUP_KEY_PASS=$KEEPITUP_KEY_PASS -t ibbaa/keepitup:build .
 docker run $DOCKER_OPTS --rm -v $PWD:/var/keepitup -w /var/keepitup ibbaa/keepitup:build /bin/bash -c "./gradlew assemble && ./signing.sh"
 RET=$?
 docker image prune -f
