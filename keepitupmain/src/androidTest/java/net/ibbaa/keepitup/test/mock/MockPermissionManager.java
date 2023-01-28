56bb4d40b889f1af06b49be449628d64d851e445
Alwin Ibba <alwin@ibba.de>
2023-01-28 18:43:00 +0100

    Implemented MockPermissionManager

---
+++ keepitupmain/src/androidTest/java/net/ibbaa/keepitup/test/mock/MockPermissionManager.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2023. Alwin Ibba
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.ibbaa.keepitup.test.mock;
+
+import androidx.annotation.NonNull;
+
+import net.ibbaa.keepitup.ui.permission.IPermissionManager;
+
+public class MockPermissionManager implements IPermissionManager {
+
+    private boolean shouldAskForRuntimePermission;
+    private boolean hasPostNotificationsPermission;
+    private boolean hasPermission;
+    private int requestPostNotificationsPermissionCalls;
+    private int requestPermissionCalls;
+
+    public MockPermissionManager() {
+        reset();
+    }
+
+    public void reset() {
+        this.shouldAskForRuntimePermission = false;
+        this.hasPostNotificationsPermission = true;
+        this.hasPermission = true;
+        this.requestPostNotificationsPermissionCalls = 0;
+        this.requestPermissionCalls = 0;
+    }
+
+    public void setShouldAskForRuntimePermission(boolean shouldAskForRuntimePermission) {
+        this.shouldAskForRuntimePermission = shouldAskForRuntimePermission;
+    }
+
+    public void setHasPostNotificationsPermission(boolean hasPostNotificationsPermission) {
+        this.hasPostNotificationsPermission = hasPostNotificationsPermission;
+    }
+
+    public void setHasPermission(boolean hasPermission) {
+        this.hasPermission = hasPermission;
+    }
+
+    public int getRequestPostNotificationsPermissionCalls() {
+        return requestPostNotificationsPermissionCalls;
+    }
+
+    public int getRequestPermissionCalls() {
+        return requestPermissionCalls;
+    }
+
+    @Override
+    public boolean shouldAskForRuntimePermission() {
+        return shouldAskForRuntimePermission;
+    }
+
+    @Override
+    public boolean hasPostNotificationsPermission() {
+        return hasPostNotificationsPermission;
+    }
+
+    @Override
+    public void requestPostNotificationsPermission() {
+        requestPostNotificationsPermissionCalls++;
+    }
+
+    @Override
+    public void requestPermission(String permission, int code) {
+        requestPermissionCalls++;
+    }
+
+    @Override
+    public boolean hasPermission(String permission) {
+        return hasPermission;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+    }
+}
