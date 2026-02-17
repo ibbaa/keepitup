bbe8883dbc151177c75821d1d4dea4adf1a6beb9
Alwin Ibba <alwin@ibba.de>
2026-02-17 20:16:03 +0100

    Encryption implementation started

--- keepitupmain/src/main/java/net/ibbaa/keepitup/resources/encryption/CipherManager.java
+++ keepitupmain/src/main/java/net/ibbaa/keepitup/resources/encryption/CipherManager.java
@@ -35,7 +35,7 @@ public class CipherManager {
     }
 
     public EncryptionResult encrypt(Map<String, String> kdfAlgorithmParam, Map<String, String> cipherAlgorithmParam, String password, String aad) {
-        Log.d(CipherManager.class.getName(), "encrypt for KDF algorithm " + kdfAlgorithmParam.toString() + ", cipher algorithm " + cipherAlgorithmParam + ", password " + password + ", aad " + aad);
+        Log.d(CipherManager.class.getName(), "encrypt for KDF algorithm " + kdfAlgorithmParam.toString() + ", cipher algorithm " + cipherAlgorithmParam + ", password " + StringUtil.maskSecret(password, true) + ", aad " + aad);
         String kdfAlgorithmName = kdfAlgorithmParam.get(getResources().getString(R.string.algorithm_json_key));
         AlgorithmData.Algorithm kdfAlgorithm = AlgorithmData.Algorithm.forName(kdfAlgorithmName);
         if (!AlgorithmData.Algorithm.ARGON2ID.equals(kdfAlgorithm)) {
@@ -47,6 +47,7 @@ public class CipherManager {
     }
 
     private byte[] createKeyWithArgon2(Map<String, String> kdfAlgorithm, String password) {
+        Log.d(CipherManager.class.getName(),  "createKeyWithArgon2 for KDF algorithm " + kdfAlgorithm.toString());
         String normalizedPassword = StringUtil.normalizeString(password);
         byte[] normalizedPasswordBytes = normalizedPassword.getBytes(StandardCharsets.UTF_8);
         return new byte[0];
